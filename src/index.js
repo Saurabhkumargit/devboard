b52a770396ae355efcd9d45dfbc20dfd4098fedf
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-04-14 15:51:54 +0530

    add patch to cors

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
   );
   res.header(
     "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
   );
 
   // ✅ handle preflight requests
